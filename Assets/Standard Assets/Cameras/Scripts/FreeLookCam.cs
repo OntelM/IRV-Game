b3b0c7cffa9d19f6bbdda026d4baec415b520cac
ontelM <33071861+ontelM@users.noreply.github.com>
2017-11-11 13:41:36 +0200

    Update

--- Assets/Standard Assets/Cameras/Scripts/FreeLookCam.cs
+++ Assets/Standard Assets/Cameras/Scripts/FreeLookCam.cs
@@ -13,7 +13,7 @@ namespace UnityStandardAssets.Cameras
         // 		Pivot
         // 			Camera
 
-        [SerializeField] private float m_MoveSpeed = 1f;                      // How fast the rig will move to keep up with the target's position.
+        [SerializeField] private float m_MoveSpeed = 15.0f;                      // How fast the rig will move to keep up with the target's position. 1
         [Range(0f, 10f)] [SerializeField] private float m_TurnSpeed = 1.5f;   // How fast the rig will rotate from user input.
         [SerializeField] private float m_TurnSmoothing = 0.0f;                // How much smoothing to apply to the turn input, to reduce mouse-turn jerkiness
         [SerializeField] private float m_TiltMax = 75f;                       // The maximum value of the x axis rotation of the pivot.
@@ -23,7 +23,7 @@ namespace UnityStandardAssets.Cameras
 
         private float m_LookAngle;                    // The rig's y axis rotation.
         private float m_TiltAngle;                    // The pivot's x axis rotation.
-        private const float k_LookDistance = 100f;    // How far in front of the pivot the character's look target is.
+        private const float k_LookDistance = 100f;    // How far in front of the pivot the character's look target is. 100f
 		private Vector3 m_PivotEulers;
 		private Quaternion m_PivotTargetRot;
 		private Quaternion m_TransformTargetRot;
