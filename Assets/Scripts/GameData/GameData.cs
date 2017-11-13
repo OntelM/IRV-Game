1ed5fd9cc8db1a02cfeef243dcc04ad064a5aa30
ontelM <33071861+ontelM@users.noreply.github.com>
2017-11-13 16:58:33 +0200

    Commit2

--- Assets/Scripts/GameData/GameData.cs
+++ Assets/Scripts/GameData/GameData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
 using System.IO;
-
+using System;
 
 [XmlRoot("GameData")]
 public class GameData{
@@ -18,6 +18,47 @@ public class GameData{
 
     [XmlElement("GameLocation")]
     public GameLocation locationId;
+
+    public static void Load()
+    {
+        Debug.Log(gameDataFile);
+
+        if (File.Exists(gameDataFile))
+        {
+            // Load info from XML
+            var serializer = new XmlSerializer(typeof(GameData));
+            var stream = new FileStream(gameDataFile, FileMode.Open);
+            try
+            {
+                Instance = serializer.Deserialize(stream) as GameData;
+                stream.Close();
+
+                Debug.Log("Gameplay loaded: " + gameDataFile);
+
+                isLoaded = true;
+
+                if (OnLoad != null)
+                {
+                    OnLoad();
+                    Debug.Log("OnLoad......................");
+                }
+
+            }
+            catch (System.Exception e)
+            {
+                stream.Close();
+                
+            }
+        }
+
+
+
+    }
+
+    private static void print(string v)
+    {
+        throw new NotImplementedException();
+    }
 
     public static void Save()
     {
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager Instance;
+
+    //public delegate void InitFunc(bool state);
+    //public static event InitFunc OnInit;
+    //public LevelInit levelInit;
+
+
+    public static GameManager Get()
+    {
+        return Instance;
+    }
+
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+            Init();
+        }
+        else
+        {
+            Destroy(gameObject);
+            Debug.Log("GameManager Instance: " + (Instance == this));
+        }
+    }
+
+    public void Init()
+    {
+        GameData.Load();
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SceneManager.LoadScene("GameLocation");
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            GameData.Load();
+            print("Load");
+        }
+
+    }
+}
